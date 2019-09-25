45d67e54e50511d9262fa308af073b3742ea8e5e
Brock Whitbread <brock.whitbread@gmail.com>
2019-09-25 14:13:08 -0700

    pass props to the PokeCard and crudely display them in that component

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const PokeCard = (props) => {
-  return(
-    <div>
-      PokeCard
+  const printableTypes = props.typesArray.map(type => {
+    return <p><strong>Type:</strong> { type.name }</p>;
+  });
+
+  return (
+    <div className="PokeCard">
+      <img src={ props.frontImage } alt={ props.name } />
+      <p>
+        <strong>Name:</strong> { props.name.charAt(0).toUpperCase() + props.name.slice(1) }
+      </p>
+      { printableTypes }
     </div>
   );
 }
--- src/components/PokeContainer.js
+++ src/components/PokeContainer.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import SearchBar from './SearchBar';
 import PokeCard from './PokeCard';
 
 class PokeContainer extends Component {
   render() {
-    let pokemon = this.props.pokemon.map(poke => <PokeCard />);
+    let pokemon = this.props.pokemon.map(poke => {
+      return(
+        <PokeCard
+          id={poke.id}
+          name={poke.name}
+          typesArray={poke.types}
+          height={poke.height}
+          weight={poke.weight}
+          backImage={poke.sprites.back_shiny}
+          frontImage={poke.sprites.front_shiny}
+        />
+      )
+    });
 
     return (
       <div>
         PokeContainer  
-        <SearchBar />
-        
+
         { pokemon }
       </div>
     );
