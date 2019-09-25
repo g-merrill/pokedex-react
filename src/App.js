384ff77142e0b9f8876228a60069cdd99255b645
Brock Whitbread <brock.whitbread@gmail.com>
2019-09-25 13:35:23 -0700

    handle the pokemon data through App state

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import './App.css';
 let data = require('./originalNine.json');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: data.pokemon
+    }
+  }
 
   render() {
 
     return (
       <div className="App">
         <Navbar />
-        <PokeContainer pokemon={data.pokemon} />
+        <PokeContainer pokemon={ this.state.pokemon } />
       </div>
     );
   }
