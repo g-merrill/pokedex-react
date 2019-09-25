592273c54f20f65dc66108ca2fe8fb68ecdbc74d
Brock Whitbread <brock.whitbread@gmail.com>
2019-09-25 14:12:21 -0700

    adds SearchBar to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import SearchBar from './components/SearchBar';
 import PokeContainer from './components/PokeContainer';
 import './App.css';
 let data = require('./originalNine.json');
@@ -17,6 +18,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar />
+        <SearchBar />
         <PokeContainer pokemon={ this.state.pokemon } />
       </div>
     );
