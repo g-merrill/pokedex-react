1e54995e25900458d911444c1f7466d3d6f0d986
Brock Whitbread <brock.whitbread@gmail.com>
2019-09-25 13:15:54 -0700

    adds SearchBar to project and rendered in PokeContainer

--- src/components/PokeContainer.js
+++ src/components/PokeContainer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar';
 
 class PokeContainer extends Component {
   render() {
     return (
       <div>
         PokeContainer  
+
+        Search: <SearchBar />
       </div>
     );
   }
